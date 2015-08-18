92d16fe05d0f26b68caea5deaf8b3e929374a748
Afif Fahmi Brilian <af.brilian@gmail.com>
2015-08-18 21:12:31 +0700

    add namedparameters jdbc template

--- src/main/java/com/springjdbc/dao/JdbcContactDao.java
+++ src/main/java/com/springjdbc/dao/JdbcContactDao.java
@@ -1,6 +1,8 @@
 package com.springjdbc.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -10,30 +12,51 @@ import com.springjdbc.sqlhandling.MySQLErrorCodesTranslator;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class JdbcContactDao implements ContactDao, InitializingBean {
 
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
-
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
 	public void afterPropertiesSet() throws Exception {
 		if (dataSource == null) {
 			throw new BeanCreationException("Must set dataSource on ContactDao");
 		}
+		/*use parameter jdbc template*/
+		if (namedParameterJdbcTemplate == null) {
+			throw new BeanCreationException("Null NamedParameterJdbcTemplate on ContactDao");
+		}
+		/*Use NON-parameter jdbc template
 		if (jdbcTemplate == null) {
 			throw new BeanCreationException("Null JdbcTemplate on ContactDao");
 		}
-
+		*/
 	}
 
 	public void setDataSource(DataSource dataSource) {
+		/*use parameter jdbc template*/
 		this.dataSource = dataSource;
+		
+		NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+		
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+		
+		/*Use NON-parameter jdbc template
+		this.dataSource = dataSource;
+		
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		jdbcTemplate.setDataSource(dataSource);
+		
 		MySQLErrorCodesTranslator errorTranslator = new MySQLErrorCodesTranslator();
+		
 		errorTranslator.setDataSource(dataSource);
+		
 		jdbcTemplate.setExceptionTranslator(errorTranslator);
+		
 		this.jdbcTemplate = jdbcTemplate;
+		*/
 	}
 
 	public List<Contact> findAll() {
@@ -52,9 +75,19 @@ public class JdbcContactDao implements ContactDao, InitializingBean {
 	}
 
 	public String findFirstNameById(int id) {
+		/*use parameter jdbc template*/
+		String sql = "select first from employees where id = :id";
+		
+		Map<String, Object> namedParameters = new HashMap<String, Object>();
+		namedParameters.put("id", id);
+		
+		return namedParameterJdbcTemplate.queryForObject(sql, namedParameters, String.class);
+		
+		/*Use NON-parameter jdbc template
 		return jdbcTemplate.queryForObject(
 				"select first from employees where id = ?",
 				new Object[] { id }, String.class);
+		*/
 	}
 
 	public void insert(Contact contact) {
